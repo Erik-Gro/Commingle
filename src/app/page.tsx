7f4a768a1328e048bd008588e3e3caaffc3b253c
Eric <eric.gro.2000@gmail.com>
2024-10-26 17:14:21 +0200

    added sign out functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useAuthActions } from "@convex-dev/auth/react";
+
 export default function Home() {
-  return <div>123</div>;
+  const { signOut } = useAuthActions();
+
+  return (
+    <div>
+      Logged in!
+      <Button
+        onClick={() => {
+          signOut();
+        }}
+      >
+        Sign out
+      </Button>
+    </div>
+  );
 }
