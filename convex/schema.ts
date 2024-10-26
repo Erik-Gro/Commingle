4c1b884cf04bbad51fb9829cc1f08ad78b49494a
Eric <eric.gro.2000@gmail.com>
2024-10-26 15:45:52 +0200

    switch to next.js 14.2.14

--- convex/schema.ts
+++
@@ -1,7 +0,0 @@
-import { v } from "convex/values";
-import { authTables } from "@convex-dev/auth/server";
-import { defineSchema, defineTable } from "convex/server";
-
-const schema = defineSchema({});
-
-export default schema;
--- src/app/auth/page.tsx
+++
@@ -1,7 +0,0 @@
-import { AuthScreen } from "../features/auth/components/AuthScreen";
-
-const AuthPage = () => {
-  return <AuthScreen />;
-};
-
-export default AuthPage;
