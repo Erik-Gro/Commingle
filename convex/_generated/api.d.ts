bb518368adbf895a9d1a6aa790919f0b3d4afcf2
Eric <eric.gro.2000@gmail.com>
2024-10-29 03:11:21 +0100

    added components and hooks

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -16,6 +16,10 @@ import type {
 import type * as auth from "../auth.js";
 import type * as channels from "../channels.js";
 import type * as http from "../http.js";
+import type * as members from "../members.js";
+import type * as messages from "../messages.js";
+import type * as reactions from "../reactions.js";
+import type * as upload from "../upload.js";
 import type * as users from "../users.js";
 import type * as workspaces from "../workspaces.js";
 
@@ -31,6 +35,10 @@ declare const fullApi: ApiFromModules<{
   auth: typeof auth;
   channels: typeof channels;
   http: typeof http;
+  members: typeof members;
+  messages: typeof messages;
+  reactions: typeof reactions;
+  upload: typeof upload;
   users: typeof users;
   workspaces: typeof workspaces;
 }>;
